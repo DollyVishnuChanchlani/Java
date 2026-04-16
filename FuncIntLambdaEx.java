50b7b5112eb0a060284379592d3acfdd16beacb5
DollyVishnuChanchlani <dtemporary89@gmail.com>
2026-04-16 22:16:43 +0530

    Update Lambda Exp

--- FuncIntLambdaEx.java
+++ FuncIntLambdaEx.java
@@ -1,11 +1,16 @@
 @FunctionalInterface //SAM(Single Abstract Method)
 interface A{
-    void show();//method is by default abstract and public
+    void show(int i);//method is by default abstract and public
 }
-
+@FunctionalInterface //SAM(Single Abstract Method)
+interface B{
+    int add(int i, int j);//method is by default abstract and public
+}
 class FuncIntLambdaEx{
     public static void main(String args[]){
-        A a = () -> System.out.println("in show");
-        a.show();
+        A a = i -> System.out.println("in show"+i);//lambda expression works only with functional interface
+        a.show(5);
+        B b = (i,j) -> i+j;//lambda expression works only with functional interface
+        System.out.println(b.add(5,4));
     }
 }
